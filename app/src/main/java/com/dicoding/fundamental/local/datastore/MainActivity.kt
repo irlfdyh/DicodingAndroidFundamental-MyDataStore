73c20b6331493fca594af7cb41467ffd2545844f
Irshal Firdeansyah <irshalfir@gmail.com>
2023-04-05 16:49:19 +0700

    implement switch action to change theme

--- app/src/main/java/com/dicoding/fundamental/local/datastore/MainActivity.kt
+++ app/src/main/java/com/dicoding/fundamental/local/datastore/MainActivity.kt
@@ -2,10 +2,24 @@ package com.dicoding.fundamental.local.datastore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val switchTheme = findViewById<SwitchMaterial>(R.id.switch_theme)
+
+        switchTheme.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switchTheme.isChecked = false
+            }
+        }
     }
 }
